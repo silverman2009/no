1495aae3f3cfbe0acb0ff78f4e38973ad7d1e6ac
Farzin Bagheri <silverman2005@gmail.com>
2022-11-19 17:51:20 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
-
+const port = process.env.PORT || 3001;
 //create a server object:
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+}).listen(port); //the server object listens on port 8080
